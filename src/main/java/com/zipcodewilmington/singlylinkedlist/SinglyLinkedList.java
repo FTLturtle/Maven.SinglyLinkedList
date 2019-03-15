ac54e3606a8fb26b37cfb6e7ceacc8e227d2f72f
Sunhyun Miller <sunhyunmiller@zipcoders-MacBook-Pro.local>
2019-03-15 09:19:18 -0400

    got rid of .idea files

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -3,5 +3,97 @@ package com.zipcodewilmington.singlylinkedlist;
 /**
  * Created by leon on 1/10/18.
  */
-public class SinglyLinkedList {
+public class SinglyLinkedList<T> {
+    private int size = 0;
+
+    private Node<T> head;
+    private Node<T> tail;
+
+    public SinglyLinkedList() {
+    }
+
+    public void add(T item) {
+        if (head == null) {
+            head = new Node<>(item, null);
+            tail = head;
+        } else {
+            tail.next = new Node<>(item, null);
+            tail = tail.next;
+        }
+        size++;
+    }
+
+    public void remove(int index) throws IndexOutOfBoundsException {
+        if (index >= size || index < 0)
+            throw new IndexOutOfBoundsException();
+
+        if (index == 0) {
+            head = head.next;
+        } else {
+            Node<T> nodeToRemove = head;
+            Node<T> last = null;
+            for (int i = 0; i < index; i++) {
+                last = nodeToRemove;
+                nodeToRemove = nodeToRemove.next;
+            }
+            last.next = nodeToRemove.next;
+        }
+    }
+
+    public Boolean contains(T item) {
+        return find(item) != -1;
+    }
+
+    public Integer find(T item) {
+        int index = 0;
+        if (item == null) {
+            for (Node<T> node = head; node != null; node = node.next) {
+                if (node.item == null) {
+                    return index;
+                }
+                index++;
+            }
+        } else {
+            for (Node<T> node = head; node != null; node = node.next) {
+                if (item.equals(node.item)) {
+                    return index;
+                }
+                index++;
+            }
+        }
+        return -1;
+    }
+
+    public Integer size() {
+        return size;
+    }
+
+    public T get(Integer index) throws IndexOutOfBoundsException {
+        if (index >= size || index < 0)
+            throw new IndexOutOfBoundsException();
+
+        Node<T> result = head;
+        for (int i = 0; i < index; i++) {
+            result = result.next;
+        }
+        return result.item;
+    }
+
+    public SinglyLinkedList<T> copy() {
+        return null;
+    }
+
+    public void sort() {
+
+    }
+
+    private static class Node<T> {
+        T item;
+        Node<T> next;
+
+        Node(T item, Node<T> next) {
+            this.item = item;
+            this.next = next;
+        }
+    }
 }
--- src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
+++ src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
@@ -1,7 +1,177 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedListTest {
+    @Test
+    public void addTest() {
+        // Given
+        Integer item = 8;
+        SinglyLinkedList<Integer> singLinkList = new SinglyLinkedList<>();
+
+        // When
+        singLinkList.add(item);
+
+        // Then
+        Assert.assertTrue(singLinkList.contains(item));
+    }
+
+    @Test
+    public void removeTest1() {
+        // Given
+        Integer item = 8;
+        SinglyLinkedList<Integer> singLinkList = new SinglyLinkedList<>();
+        singLinkList.add(item);
+        singLinkList.add(89);
+        singLinkList.add(45);
+
+        // When
+        singLinkList.remove(0);
+
+        // Then
+        Assert.assertFalse(singLinkList.contains(item));
+    }
+
+    @Test
+    public void removeTest2() {
+        // Given
+        Integer item = 8;
+        SinglyLinkedList<Integer> singLinkList = new SinglyLinkedList<>();
+        singLinkList.add(89);
+        singLinkList.add(45);
+        singLinkList.add(item);
+
+
+        // When
+        singLinkList.remove(2);
+
+        // Then
+        Assert.assertFalse(singLinkList.contains(item));
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void removeTest3() {
+        // Given
+        SinglyLinkedList<Integer> singLinkList = new SinglyLinkedList<>();
+        singLinkList.add(89);
+        singLinkList.add(45);
+        singLinkList.add(45);
+
+        // When
+        singLinkList.remove(3);
+    }
+
+    @Test
+    public void findTest2() {
+        // Given
+        Integer item = 8;
+        SinglyLinkedList<Integer> singLinkList = new SinglyLinkedList<>();
+        singLinkList.add(89);
+        singLinkList.add(45);
+        singLinkList.add(item);
+        singLinkList.add(987);
+        Integer expectedIndex = 2;
+
+        // When
+        Integer actualIndex = singLinkList.find(item);
+
+        // Then
+        Assert.assertEquals(expectedIndex, actualIndex);
+    }
+
+    @Test
+    public void findTest3() {
+        // Given
+        Integer item = 8;
+        SinglyLinkedList<Integer> singLinkList = new SinglyLinkedList<>();
+        singLinkList.add(89);
+        singLinkList.add(45);
+        singLinkList.add(item);
+        singLinkList.add(987);
+        Integer expectedIndex = -1;
+
+        // When
+        Integer actualIndex = singLinkList.find(5);
+
+        // Then
+        Assert.assertEquals(expectedIndex, actualIndex);
+    }
+
+    @Test
+    public void findTest4() {
+        // Given
+        Integer item = null;
+        SinglyLinkedList<Integer> singLinkList = new SinglyLinkedList<>();
+        singLinkList.add(89);
+        singLinkList.add(45);
+        singLinkList.add(9834);
+        singLinkList.add(item);
+        Integer expectedIndex = 3;
+
+        // When
+        Integer actualIndex = singLinkList.find(item);
+
+        // Then
+        Assert.assertEquals(expectedIndex, actualIndex);
+    }
+
+    @Test
+    public void sizeTest() {
+        // Given
+        SinglyLinkedList<Integer> singLinkList = new SinglyLinkedList<>();
+        singLinkList.add(89);
+        singLinkList.add(45);
+        singLinkList.add(8);
+        singLinkList.add(987);
+        Integer expectedSize = 4;
+
+        // When
+        Integer actualSize = singLinkList.size();
+
+        // Then
+        Assert.assertEquals(expectedSize, actualSize);
+    }
+
+    @Test
+    public void getTest1() {
+        // Given
+        Integer expectedItem = 8;
+        SinglyLinkedList<Integer> singLinkList = new SinglyLinkedList<>();
+        singLinkList.add(89);
+        singLinkList.add(45);
+        singLinkList.add(expectedItem);
+        singLinkList.add(987);
+        Integer index = 2;
+
+        // When
+        Integer actualItem = singLinkList.get(index);
+
+        // Then
+        Assert.assertEquals(expectedItem, actualItem);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void getTest2() {
+        // Given
+        SinglyLinkedList<Integer> singLinkList = new SinglyLinkedList<>();
+        singLinkList.add(89);
+        singLinkList.add(45);
+        singLinkList.add(98);
+        singLinkList.add(987);
+
+        // When
+        Integer item = singLinkList.get(5);
+    }
+
+    @Test
+    public void copy() {
+    }
+
+    @Test
+    public void sort() {
+    }
 }
